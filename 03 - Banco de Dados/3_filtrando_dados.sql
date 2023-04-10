acf3a6c7f11269721ac722d669b7c33ba4aa0aca
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-04-10 19:56:15 -0300

    Aula 10/04

--- 03 - Banco de Dados/3_filtrando_dados.sql
+++ 03 - Banco de Dados/3_filtrando_dados.sql
@@ -9,12 +9,84 @@ SELECT * FROM Clientes WHERE Idade > 22;
 
 SELECT * FROM Clientes WHERE Idade >= 22;
 
--- filtrando com mais de uma condição usando Operador E (AND)
+-- filtrando com mais de uma condiï¿½ï¿½o usando Operador E (AND)
 
-SELECT * FROM Clientes WHERE Idade > 22 AND Nome = 'Seu zé'
+SELECT * FROM Clientes
+	WHERE Idade > 22 AND Nome = 'Seu zÃ©';
 
 
--- filtrando com mais de uma condição usando Operador OU (OR)
+-- filtrando com mais de uma condiï¿½ï¿½o usando Operador OU (OR)
 
-SELECT * FROM Clientes WHERE Idade > 22 OR Nome = 'João'
+SELECT * FROM Clientes WHERE Idade > 22 OR Nome = 'JoÃ£o';
 
+
+-- Obtendo o cliente com a menor idade
+
+SELECT Min(Idade) FROM Clientes;
+
+-- Obter o cliente com a maior idade
+
+SELECT Max(Idade) FROM Clientes;
+
+-- contando clientes
+
+SELECT COUNT(*) FROM Clientes;
+
+-- obtendo mÃ©dia de idade dos clientes
+
+SELECT AVG(Idade) AS Media_Idade FROM CLIENTES;
+
+-- Somando idade dos clientes
+
+SELECT SUM(Idade) FROM CLIENTES;
+
+-- Obter XXX registros da tabela clientes
+
+SELECT TOP 2 * FROM Clientes;
+
+-- filtrando nomes que comeÃ§am com a letra 'j'
+
+SELECT * FROM Clientes WHERE Nome LIKE 'j%';
+
+-- filtrando nomes que terminam com a letra 'o'
+
+SELECT * FROM Clientes WHERE Nome LIKE '%o';
+
+-- filtrando nomes que tenham 'Guilherme'
+
+SELECT * FROM Clientes WHERE Nome LIKE '%Guilherme%';
+
+-- Obtendo clientes em que a idade Ã© 25 ou 18
+-- Usando operador lÃ³gico
+SELECT * FROM Clientes where Idade = 25 OR Idade = 18
+-- Usando IN
+SELECT * FROM Clientes where Idade IN (25, 18)
+
+-- Usando Between para obter entre um range de Idade
+-- Usando operador lÃ³gico
+SELECT * FROM Clientes Where Idade >= 25 AND Idade <= 80;
+-- Usando BETWEEN
+SELECT * FROM Clientes Where Idade BETWEEN 25 AND 80;
+
+
+-- Order By
+-- Obter os clientes ordenados pela idade (ordem crescente)
+-- (A palavra ASC Ã© opcional pois por padrÃ£o o SQL Server jÃ¡ ordena crescente)
+SELECT * FROM Clientes ORDER BY Idade ASC;
+
+-- Obter os clientes ordenados pela idade (ordem decrescente)
+SELECT * FROM Clientes ORDER BY Idade DESC;
+
+-- ordenando por uma coluna textual
+
+SELECT * FROM Clientes ORDER BY Nome;
+
+-- Group By
+
+-- obter idade e e quantidade de clientes
+
+-- Agrupo todos os clientes pela Idade e conto quantos possuem essa determinada idade
+SELECT 
+	Idade,
+	COUNT(*)
+FROM Clientes GROUP BY Idade;
---
+++ 03 - Banco de Dados/Exercícios/Capitúlos 1 a 6/exercicios.sql
@@ -0,0 +1,92 @@
+
+-- Exercícios
+
+-- 1. Criar uma base de dados chamada: exercicio
+
+
+
+-- 2. Selecionar a base criada anteriormente
+
+
+
+-- 3. Criar uma tabela chamada uzuarios, com a seguinte estrutura:
+        -- a. nome varchar de 20 caracteres
+        -- b. sobrenome de 40 caracteres
+        -- c. idade do tipo inteiro
+
+
+
+-- a. nome varchar de 20 caracteres
+
+
+
+-- b. sobrenome de 40 caracteres
+
+
+
+-- c. idade do tipo inteiro
+
+
+
+-- 4. Renomeie a tabela uzuarios para usuarios.
+
+
+
+-- 5. Adicione uma coluna e-mail com suporte até 40 caracteres
+
+
+
+-- 6. Exclua a coluna sobrenome
+
+
+
+-- 7. Cadastre os seguintes dados:
+    /*
+    Nome, Idade, Email
+    Vanessa 16 vanessa.rosa@gmail.com
+    Adailton 22 adailton.mas@yahoo.com
+    Andressa 36 andressa.simas@uol.com.br
+    Mayra 24 mayra_antunes@gmail.com
+    Cristiane 14 cris.maya@gmail.com
+    Carina 27 carina.almeida@gmail.com
+    Clóvis 29 clovis.simao@hotmail.com
+    Gabriela 23 gabriela.bragantino@live.com
+    Cibele null cibele_lins@uol.com.br
+    */
+
+
+-- 8. Exiba quantos registros existem na tabela
+
+
+
+-- 9. Exibir a quantidade de usuários com idade até 17 anos
+
+
+
+-- 10.Retornar a quantidade de usuários com o e-mail gmail
+
+
+
+-- 11.Retornar o nome e a idade da pessoa mais velha
+
+
+
+-- 12.Retornar os dados do usuário com idade igual a nulo
+
+
+
+-- 13.Alterar para 27 todas as idades nulas
+
+
+
+-- 14.Deletar todos os usuários com idade inferior a 18 anos
+
+
+
+-- 15.Excluir todos os dados da tabela e reiniciar a contabilização
+
+
+
+-- 16.Excluir a base de dados
+
+
