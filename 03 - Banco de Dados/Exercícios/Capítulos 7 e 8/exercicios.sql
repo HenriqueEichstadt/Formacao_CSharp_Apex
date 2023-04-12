a588584049aee50b54ea350279ae9a5d7b6d7c60
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-04-12 20:48:04 -0300

    Aula 12/04

---
+++ 03 - Banco de Dados/Exercícios/Capítulos 7 e 8/exercicios.sql
@@ -0,0 +1,52 @@
+
+
+-- 1. Criar uma base de dados chamada exercicio_joins
+
+
+
+
+-- 2. Implemente a tabela cursos, com os seguintes campos:
+-- Código   INT             IDENTITY e PK
+-- Curso    VARCHAR(20)
+
+
+-- 3. Desenvolva a tabela clientes, com os seguintes campos:
+-- Código       INT         IDENTITY e PK
+-- Cliente      VARCHAR(30)
+-- Codigo_Curso INT FK
+
+
+
+-- 4. Cadastre os cursos:
+    -- 1 Java
+    -- 2 C#
+    -- 3 Python
+    -- 4 PHP
+    -- 5 Node.js
+
+
+
+-- 5. Cadastre os seguintes clientes:
+    -- 1 Larissa 3
+    -- 2 Gabriel 1
+    -- 3 Jean 1
+    -- 4 Gabriella 2
+    -- 5 Robson 3
+    -- 6 Isabella 3
+    -- 7 Eduardo 2
+    -- 8 Juliana 3
+    -- 9 Carlos 2
+    -- 10 Lorena 1
+
+
+
+-- 6. Liste o nome dos clientes e o nome dos cursos que cada um está participando.
+
+
+
+-- 7. Contar a quantidade de cursos adquiridos pelos clientes. Exiba o nome e a
+--    quantidade desses cursos. Será obrigatório a exibição de todos os cursos,
+--    independente se há clientes realizando esse curso.
+
+
+
