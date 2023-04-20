e39bb7fdfd3b2c1f337cda9c68d30adbcb0467bb
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-04-19 21:57:01 -0300

    Aula 19/04

---
+++ 04 - C# Fundamentos/CSharp_Fundamentos_Solution/Capitulo2.PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Capitulo2.PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
