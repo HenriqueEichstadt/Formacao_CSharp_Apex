ace507bf99dbda3cd56a0a6cd347f50b8e96055c
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-04-22 17:10:04 -0300

    Aula 19/04

---
+++ 04 - C# Fundamentos/Charp_Fundamentos_Solution/Capitulo4.Exercicios/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Capitulo4.Exercicios
+{
+    internal class Program
+    {
+        /*
+         1) Escolha um tipo adequado para guardar cada um dos dados em memória 
+            usando os seguintes tipos: 'int', 'string', 'bool', 'double' e 'char':
+                    
+            Crie variáveis para guardar e imprima os seguintes dados:
+
+            - “Blumenau”
+            - false
+            - 1992
+            - '1'
+            - “1”
+            - 2790.32
+            - 555
+            - true
+         
+         */
+        static void Main(string[] args)
+        {
+            // Exemplo
+            string cidade = "Blumenau";
+            Console.WriteLine(cidade);
+
+            // continue...
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
