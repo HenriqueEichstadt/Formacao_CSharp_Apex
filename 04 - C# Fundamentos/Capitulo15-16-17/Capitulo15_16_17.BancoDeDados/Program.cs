77848ce30f3f0c48c6294eca4da89f0ade591fae
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-05-11 21:30:57 -0300

    Aula 11/05

--- 04 - C# Fundamentos/Capitulo15-16-17/Capitulo15_16_17.BancoDeDados/Program.cs
+++ 04 - C# Fundamentos/Capitulo15-16-17/Capitulo15_16_17.BancoDeDados/Program.cs
@@ -1,11 +1,13 @@
 ﻿
 using System.Data.SqlClient;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Capitulo15_16_17.BancoDeDados
 {
 	public class Program
 	{
-		/*
+
+        /*
 			CREATE DATABASE Aula11052023;
 
 			CREATE TABLE Usuarios (
@@ -21,16 +23,178 @@ namespace Capitulo15_16_17.BancoDeDados
 				('João', 18, 'joao@email.com'),
 				('Maria', 25, 'maria@email.com');
 		*/
-		static void Main(string[] args)
+
+        // conectar sem senha
+        //Data Source=HENRIQUEEICHSTA\\SQLEXPRESS;Initial catalog=Aula11052023;Trusted_connection=true;trustservercertificate=true
+
+        // Conectar com senha
+        //"Server=HENRIQUEEICHSTA\\SQLEXPRESS;Database=Aula11052023;User Id=sa;Password=123456789;"
+
+        static void Main(string[] args)
 		{
-            // conectar sem senha
-            //Data Source=HENRIQUEEICHSTA\\SQLEXPRESS;Initial catalog=Aula11052023;Trusted_connection=true;trustservercertificate=true
+            // buscar
+            //SelectNoBanco();
 
-            // Conectar com senha
-            //"Server=HENRIQUEEICHSTA\\SQLEXPRESS;Database=Aula11052023;User Id=sa;Password=123456789;"
+            // inserir
+            /*var usuario = new Usuario();
+            usuario.Nome = "Teste";
+            usuario.Email = "teste@teste.com";
+            usuario.Idade = 12;
+
+            InserirNoBanco(usuario);*/
+
+
+            // atualizar
+            /*var usuario = new Usuario();
+            usuario.Id = 1;
+            usuario.Nome = "Francisco";
+            usuario.Email = "francisco@teste.com";
+            usuario.Idade = 112;
+
+            
+            AtualizarNoBanco(usuario);*/
+
+
+            // deletar
+            DeletarNoBanco(3);
+        }
+
+        public static void DeletarNoBanco(int idUsuario)
+        {
             var connectionString = "Server=HENRIQUEEICHSTA\\SQLEXPRESS;Database=Aula11052023;User Id=sa;Password=123456789;";
-			SqlConnection conexao = new SqlConnection(connectionString);
+            SqlConnection conexao = new SqlConnection(connectionString);
 
+            try
+            {
+                conexao.Open();
+
+                var comandoDelete = @"DELETE FROM Usuarios
+                                    WHERE Id = @Id";
+
+                SqlCommand comando = new SqlCommand(comandoDelete, conexao);
+                comando.Parameters.AddWithValue("@Id", idUsuario);
+
+                var linhasAfetadas = comando.ExecuteNonQuery();
+
+                Console.WriteLine("Linhas afetadas: " + linhasAfetadas);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public static void AtualizarNoBanco(Usuario usuario)
+        {
+            var connectionString = "Server=HENRIQUEEICHSTA\\SQLEXPRESS;Database=Aula11052023;User Id=sa;Password=123456789;";
+            SqlConnection conexao = new SqlConnection(connectionString);
+
+            try
+            {
+                conexao.Open();
+
+                var comandoUpdate = @"UPDATE Usuarios
+                                    SET Nome = @Nome,
+                                        Idade = @Idade,
+                                        Email = @Email
+                                    WHERE Id = @Id";
+
+                SqlCommand comando = new SqlCommand(comandoUpdate, conexao);
+                comando.Parameters.AddWithValue("@Id", usuario.Id);
+                comando.Parameters.AddWithValue("@Nome", usuario.Nome);
+                comando.Parameters.AddWithValue("@Idade", usuario.Idade);
+                comando.Parameters.AddWithValue("@Email", usuario.Email);
+
+                var linhasAfetadas = comando.ExecuteNonQuery();
+
+                Console.WriteLine("Linhas afetadas: " + linhasAfetadas);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+
+        public static void InserirNoBanco(Usuario usuario)
+		{
+            var connectionString = "Server=HENRIQUEEICHSTA\\SQLEXPRESS;Database=Aula11052023;User Id=sa;Password=123456789;";
+            SqlConnection conexao = new SqlConnection(connectionString);
+
+            try
+            {
+                conexao.Open();
+
+                var comandoInsert = @"INSERT INTO Usuarios (Nome, Idade, Email) 
+                                       VALUES(@Nome, @Idade, @Email)";
+
+                SqlCommand comando = new SqlCommand(comandoInsert, conexao);
+                comando.Parameters.AddWithValue("@Nome", usuario.Nome);
+                comando.Parameters.AddWithValue("@Idade", usuario.Idade);
+                comando.Parameters.AddWithValue("@Email", usuario.Email);
+
+                var linhasAfetadas = comando.ExecuteNonQuery();
+
+                Console.WriteLine("Linhas afetadas: " + linhasAfetadas);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+
+		public static void SelectNoBanco()
+		{
+            var connectionString = "Server=HENRIQUEEICHSTA\\SQLEXPRESS;Database=Aula11052023;User Id=sa;Password=123456789;";
+            SqlConnection conexao = new SqlConnection(connectionString);
+
+            try
+            {
+                conexao.Open();
+
+                var comando = "SELECT Id, Nome, Idade, Email FROM Usuarios";
+
+                SqlCommand comandoBusca = new SqlCommand(comando, conexao);
+
+                SqlDataReader reader = comandoBusca.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"Id: {reader["Id"]}");
+                    Console.WriteLine($"Nome: {reader["Nome"]}");
+                    Console.WriteLine($"Idade: {reader["Idade"]}");
+                    Console.WriteLine($"Email: {reader["Email"]}");
+
+                    Console.WriteLine();
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
         }
 	}
 }
---
+++ 04 - C# Fundamentos/Capitulo15-16-17/Capitulo15_16_17.BancoDeDados/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capitulo15_16_17.BancoDeDados
+{
+    /*
+	Id INT IDENTITY(1, 1),
+	Nome VARCHAR(30),
+	Idade INT,
+	Email VARCHAR(40),
+	PRIMARY KEY(Id)
+	*/
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Email { get; set; }
+    }
+}
