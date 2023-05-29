7808bd4c8cac84362e8f260db831c5dfd539d372
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-05-29 19:42:48 -0300

    Aula 29/05

---
+++ 05 - ASP.NET Core/Capitulo 04/MeuProjetoMVC/MeuProjetoMVC/BancoDeDados/Configs/PessoaConfig.cs
@@ -0,0 +1,30 @@
+﻿using MeuProjetoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MeuProjetoMVC.BancoDeDados.Configs
+{
+    public class PessoaConfig : IEntityTypeConfiguration<Pessoa>
+    {
+        public void Configure(EntityTypeBuilder<Pessoa> builder)
+        {
+            // definindo o nome da tabela
+            builder.ToTable("Pessoas");
+
+            // definindo a coluna principal (primary key)
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            // mapeando as propriedades
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Cpf)
+                .IsRequired()
+                .HasMaxLength(14);
+
+
+        }
+    }
+}
---
+++ 05 - ASP.NET Core/Capitulo 04/MeuProjetoMVC/MeuProjetoMVC/BancoDeDados/Contexto/BancoDeDadosContext.cs
@@ -0,0 +1,27 @@
+﻿using MeuProjetoMVC.BancoDeDados.Configs;
+using MeuProjetoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuProjetoMVC.BancoDeDados.Contexto
+{
+    public class BancoDeDadosContext : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = "Server=HENRIQUEEICHSTA\\SQLEXPRESS;Database=MeuProjetoMVC;User Id=sa;Password=123456789;TrustServerCertificate=True;";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // importart as configs
+            modelBuilder.ApplyConfiguration(new PessoaConfig());
+        }
+
+    }
+}
