afd573c4b769cde5f03bcf6db5735ea802d24477
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-06-05 21:07:33 -0300

    Aula 05/06

---
+++ 06 - API .NET e Angular/MeuProjetoApi/MeuProjetoApi/Controllers/PessoaController.cs
@@ -0,0 +1,53 @@
+﻿using MeuProjetoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MeuProjetoApi.Controllers
+{
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        public static List<Pessoa> ListaPessoas = new List<Pessoa>() 
+        {
+            new Pessoa() { Id = 1, Nome = "Zé", Cpf = "000.000.000-00", Email = "ze@gmail.com", Telefone = "(47) 99874-5632" },
+        };
+
+
+        [HttpGet]
+        [Route("pessoa/obter")]
+        [ProducesResponseType(typeof(Pessoa), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(Nullable), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.InternalServerError)]
+        public IActionResult Obter()
+        {
+            try
+            {
+                var pessoa = new Pessoa();
+                pessoa.Id = 1;
+                pessoa.Nome = "Zé";
+                pessoa.Cpf = "000.000.000-00";
+                pessoa.Email = "ze@gmail.com";
+                pessoa.Telefone = "(47) 99874-5632";
+
+                return Ok(pessoa);
+
+            } catch (Exception ex)
+            {
+                return BadRequest($"Erro na API: {ex.Message} - {ex.StackTrace}");
+            }
+        }
+
+        [HttpPost]
+        [Route("pessoa/adicionar")]
+        [ProducesResponseType(typeof(Pessoa), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(Nullable), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.InternalServerError)]
+        public IActionResult Adicionar()
+        {
+            return Created(null);
+        }
+
+
+
+    }
+}
