547f513ec2f196da7e53cb6f54f84964667c61ba
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-06-07 20:33:47 -0300

    Aula 07/06

---
+++ 06 - API .NET e Angular/MeuProjetoApi/MeuProjetoApi/BancoDados/Configs/PessoaConfig.cs
@@ -0,0 +1,30 @@
+﻿using MeuProjetoApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MeuProjetoApi.BancoDados.Configs
+{
+    public class PessoaConfig : IEntityTypeConfiguration<Pessoa>
+    {
+        public void Configure(EntityTypeBuilder<Pessoa> builder)
+        {
+            builder.ToTable("Pessoa");
+
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).UseIdentityColumn();
+
+            // mapeando as propriedades
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Cpf)
+                .IsRequired()
+                .HasMaxLength(14);
+
+            builder.Property(x => x.Telefone)
+                .HasMaxLength(30);
+
+        }
+    }
+}
