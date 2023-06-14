e83d60ccd815502199bc6f572475657c61628a6d
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-06-14 18:57:00 -0300

    Aula 14/06

--- 06 - API .NET e Angular/WebApp/meu-projeto-web/src/app/app.component.ts
+++ 06 - API .NET e Angular/WebApp/meu-projeto-web/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'meu-projeto-web';
+
+  public titlee: string = 'Meu projeto WEB 123';
+
+  teste = 'teste 123';
+
+  public deveRenderizarPalavraRecursos: boolean = false;
+
+  public imprimir(): void {
+    alert('Imprimir foi clicado');
+  }
+
+  public soma(valor1: number, valor2: number): number {
+    return valor1 + valor2;
+  }
+
+  public metodoQualquer(parametro: any): any {
+
+  }
+
 }
