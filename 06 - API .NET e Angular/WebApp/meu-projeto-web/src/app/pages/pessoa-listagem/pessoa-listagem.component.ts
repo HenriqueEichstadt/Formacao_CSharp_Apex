bc114e149dd26751d2b62d130b6b6c8ba258973d
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-06-14 22:23:33 -0300

    Aula 14/06

--- 06 - API .NET e Angular/WebApp/meu-projeto-web/src/app/pages/pessoa-listagem/pessoa-listagem.component.ts
+++ 06 - API .NET e Angular/WebApp/meu-projeto-web/src/app/pages/pessoa-listagem/pessoa-listagem.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Pessoa from 'src/app/models/pessoa.model';
 
 @Component({
   selector: 'app-pessoa-listagem',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pessoa-listagem.component.css']
 })
 export class PessoaListagemComponent implements OnInit {
+
+  public mockPessoas: Pessoa[] = [
+    new Pessoa({id: 1, nome: 'pessoa fake 1', cpf: '000.000.000-00', email: 'pessoa1@email.com', telefone: '(47) 99999-9999'}),
+    new Pessoa({id: 2, nome: 'pessoa fake 2', cpf: '111.000.000-00', email: 'pessoa2@email.com', telefone: '(47) 98999-9999'}),
+    new Pessoa({id: 3, nome: 'pessoa fake 3', cpf: '000.222.000-00', email: 'pessoa3@email.com', telefone: '(47) 97999-9999'}),
+    new Pessoa({id: 4, nome: 'pessoa fake 4', cpf: '000.000.333-00', email: 'pessoa4@email.com', telefone: '(47) 96999-9999'}),
+    new Pessoa({id: 5, nome: 'pessoa fake 5', cpf: '000.000.000-44', email: 'pessoa5@email.com', telefone: '(47) 95999-9999'}),
+  ];
 
   constructor() { }
 
