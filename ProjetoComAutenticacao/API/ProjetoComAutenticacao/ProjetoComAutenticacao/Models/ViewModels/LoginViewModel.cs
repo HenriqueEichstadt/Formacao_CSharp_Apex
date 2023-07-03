4f1d6e1db298f6ea79613fd6ef458d02438e8507
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2023-07-03 10:13:41 -0300

    Melhorando menu principal

--- ProjetoComAutenticacao/API/ProjetoComAutenticacao/ProjetoComAutenticacao/Models/ViewModels/LoginViewModel.cs
+++ ProjetoComAutenticacao/API/ProjetoComAutenticacao/ProjetoComAutenticacao/Models/ViewModels/LoginViewModel.cs
@@ -4,4 +4,6 @@ public class LoginViewModel
 {
     public string Token { get; set; }
     public DateTime DataExpiracao { get; set; }
+    public string NomeUsuario { get; set; }
+    public string Email { get; set; }
 }
